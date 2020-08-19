0058f9319cf1d94c51eb8a0f4a2213856082c06c
Christian Otu <christianotu.d@gmail.com>
2020-08-19 18:50:42 +0100

    Updates

---
+++ precache-manifest.0f51527a2db1e14f7c7932ff3958a6c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc3bebbe6b6a0646fa764911dd81d62",
+    "url": "/Skypie-Chat-App/index.html"
+  },
+  {
+    "revision": "89976e545f3949d3c07c",
+    "url": "/Skypie-Chat-App/static/css/main.20702f1a.chunk.css"
+  },
+  {
+    "revision": "31a707bd95e01b10601c",
+    "url": "/Skypie-Chat-App/static/js/2.18d5ca5c.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/Skypie-Chat-App/static/js/2.18d5ca5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89976e545f3949d3c07c",
+    "url": "/Skypie-Chat-App/static/js/main.38cee121.chunk.js"
+  },
+  {
+    "revision": "52f388b7be3d2c5848d2",
+    "url": "/Skypie-Chat-App/static/js/runtime-main.fc3c7cac.js"
+  }
+]);
